9d8345b8b6e7bb1fe48632fed3e50ab37d7bcf71
Nishpatil1 <142910741+Nishpatil1@users.noreply.github.com>
2023-09-03 20:03:59 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,143 @@
+  public class Mobile{
+
+  public static void main(String[] args){
+
+
+        String mobilename="apple";
+	int mobileprice=1200000;
+	String mobilecolour="rosegold";
+	String mobileRAM="6GB";
+
+	System.out.println(mobilename);
+	System.out.println(mobileprice);
+	System.out.println(mobilecolour);
+	System.out.println(mobileRAM);
+
+	String Tvname="Samsung"
+	int Tvprice=261000000;
+	String Tvcolour="black";
+	String Tvinch="55";
+
+	System.out.println(Tvname);
+	System.out.println(Tvprice);
+	System.out.println(Tvcolour);
+	System.out.println(Tvinch);
+
+	String Washingmachinename="Haier"
+	int Washingmachineprice=250000000;
+	String Washingmachinecolour="red";
+	boolean isWashingmachineworking="true";
+
+	System.out.println(Washingmachinename);
+	System.out.println(Washingmachineprice);
+	System.out.println(Washingmachinecolour);
+	System.out.println(isWashingmachineworking);
+
+	String Goldname="Whitegold"
+	int Goldprice=260000000;
+	String Goldcolour="White";
+	String Goldweight="100gram";
+
+	System.out.println(Goldname);
+	System.out.println(Goldprice);
+	System.out.println(Goldcolour);
+	System.out.println(Goldweight);
+
+	String Fridgename="LG";
+	int Fridgeprice=256700000;
+	String Fridgecolour="Gray";
+	boolean isfridgeworking="False";
+
+	System.out.println(fridgename);
+	System.out.println(fridgeprice);
+	System.out.println(fridgecolour);
+	System.out.println(isfridgeworking);
+
+	String Watchname="Titan";
+	int Watchprice=256700000;
+	String Watchcolour="Gray";
+	boolean iswatchworking="True";
+
+	System.out.println(Watchname);
+	System.out.println(Watchprice);
+	System.out.println(Watchcolour);
+	System.out.println(iswatchworking);
+
+	String Bottlename="Milton";
+	int Bottleprice=234587900;
+	String Bottlecolour="Green";
+	String Bottlecapacity="1lt";
+
+	System.out.println(Bottlename);
+	System.out.println(Bottleprice);
+	System.out.println(Bottlecolour);
+	System.out.println(Bottlecapacity);
+
+	String Laptopname="Lenovo";
+	int Laptopprice=234000000;
+	String Laptopcolour="black";
+	String LaptopRAM="8GB";
+
+	System.out.println(Laptopname);
+	System.out.println(Laptopprice);
+	System.out.println(Laptopcolour);
+	System.out.println(LaptopRAM);
+
+	String Vegetablename="Carrot"
+	int Vegetableprice=234078000;
+	String Vegetablecolour="Orange";
+
+	System.out.println(Vegetablename);
+	System.out.println(Vegetableprice);
+	System.out.println(Vegetablecolour);
+
+	String Tubelightname="Philips";
+	int Tubelightprice=289756002;
+	String Tubelightcolour="Yellow";
+	String Tubelightvolt="40volt";
+
+	System.out.println(Tubelightname);
+	System.out.println(Tubelightprice);
+	System.out.println(Tubelightcolour);
+	System.out.println(Tubelightvolt);
+
+}
+}
+
+	
+
+
+	
+
+
+	
+
+
+
+
+
+
+
+	
+
+	
+	
+
+	
+
+
+	
+
+
+	
+	
+
+	
+
+	
+
+
+
+	
+	
+	
